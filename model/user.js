d9c5ef71fe140ba1515cce0ae83d57b937562558
15gk <gautamkriti89@gmail.com>
2024-01-02 23:55:27 +0545

    camera and detection is connected

---
+++ model/user.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema(
+  {
+    userId: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    fullName: {
+      type: String,
+    },
+    phoneNumber: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+UserSchema.plugin(passportLocalMongoose);
+
+const UserModel = mongoose.model("user", UserSchema);
+module.exports = UserModel;
